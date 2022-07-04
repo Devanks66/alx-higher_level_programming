4de32620fe56e023c394bdf30b8bac23d4a1226e
Theophilus Ankamah <theoankamah66@gmail.com>
2022-07-04 13:30:14 -0700

    task6

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,7 +1,3 @@
 #!/usr/bin/python3
 def no_c(my_string):
-    new_string = ""
-    for k in my_string:
-        if k != "c" and k != "C":
-            new_string = new_string + c
-    return(new_string)
+    return ("".join(c for c in my_string if c not in "Cc"))
